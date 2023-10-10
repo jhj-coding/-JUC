898f7b35a5017013f592165f8bd663cf3ae25c60
jhj <2539111904@qq.com>
2023-10-10 15:06:22 +0800

    2

--- Leetcode/src/main/java/com/jhj/algorithm/nowcoder/top101.java
+++ Leetcode/src/main/java/com/jhj/algorithm/nowcoder/top101.java
@@ -32,4 +32,49 @@ public class top101 {
             return pre;
         }
     }
+
+    //BM2 链表内指定区间反转
+    public class Solution2 {
+        public class ListNode {
+            int val;
+            ListNode next = null;
+
+            public ListNode(int val) {
+                this.val = val;
+            }
+        }
+
+        /**
+         * 代码中的类名、方法名、参数名已经指定，请勿修改，直接返回方法规定的值即可
+         *
+         * @param head ListNode类
+         * @param m    int整型
+         * @param n    int整型
+         * @return ListNode类
+         */
+        public ListNode reverseBetween(ListNode head, int m, int n) {
+            // write code here
+            ListNode pre=new ListNode(0);
+            pre.next=head;
+            ListNode cur=head;
+            ListNode pree=pre;
+            for(int i=1;i<m;i++){
+                pree=pree.next;
+                cur=cur.next;
+            }
+            ListNode next=cur.next;
+            for (int i=m;i<n;i++){
+                next=next.next;
+            }
+            for(int i=m;i<n;i++){
+                ListNode cur1 = cur.next;
+                cur.next=next;
+                next=cur;
+                cur=cur1;
+            }
+            cur.next=next;
+            pree.next=cur;
+            return pre.next;
+        }
+    }
 }
