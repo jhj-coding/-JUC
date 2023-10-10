8815afe8f0f9dd4d418e6fbb33798dc824c87743
jhj <2539111904@qq.com>
2023-10-10 16:34:37 +0800

    2

--- Leetcode/src/main/java/com/jhj/sql/nowcoder/1.sql
+++ Leetcode/src/main/java/com/jhj/sql/nowcoder/1.sql
@@ -1,2 +1,5 @@
 #SQL1 查询所有列
 select * from user_profile;
+
+#SQL2 查询多列
+select device_id,gender,age,university from user_profile
--- Leetcode/src/main/java/com/jhj/sql/nowcoder/all.sql
+++ Leetcode/src/main/java/com/jhj/sql/nowcoder/all.sql
@@ -1,2 +1,4 @@
 #SQL195 查找最晚入职员工的所有信息
 select * from employees order by hire_date desc limit 1;
+#SQL196 查找入职员工时间排名倒数第三的员工所有信息 distinct去重
+select * from employees where hire_date =(select distinct hire_date from employees order by hire_date desc limit 2,1);
