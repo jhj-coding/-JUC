27653a3d8fff709ff1aaee7f398d03c7ac96a1b7
jhj <2539111904@qq.com>
2023-10-10 16:54:18 +0800

    2

---
+++ Leetcode/src/main/java/com/jhj/shell/nowcoder/all.sh
@@ -0,0 +1,10 @@
+#SHELL1 统计文件的行数 wc 命令用于统计文件字节（-c）、字符（-m）、单词（-w）与行（-l）的数量。
+# | 是将前一个命令结果给后一个命令
+# awk 是一个脚本 $1 第一个字段
+#https://blog.csdn.net/sinat_32152141/article/details/125133897
+#https://zhuanlan.zhihu.com/p/627048291?utm_id=0
+wc -l nowcoder.txt | awk '{print $1}'
+
+#tail 显示尾部几行
+#sed 命令打印第几行 https://blog.csdn.net/Logicr/article/details/112845375
+tail -n 5 nowcoder.txt
