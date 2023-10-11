f63bce3aff10590e30547f81c3740ccb3046ad19
jhj <2539111904@qq.com>
2023-10-11 15:22:44 +0800

    3

--- Leetcode/src/main/java/com/jhj/algorithm/nowcoder/top101.java
+++ Leetcode/src/main/java/com/jhj/algorithm/nowcoder/top101.java
@@ -54,26 +54,73 @@ public class top101 {
          */
         public ListNode reverseBetween(ListNode head, int m, int n) {
             // write code here
+            ListNode pre = new ListNode(0);
+            pre.next = head;
+            ListNode cur = head;
+            ListNode pree = pre;
+            for (int i = 1; i < m; i++) {
+                pree = pree.next;
+                cur = cur.next;
+            }
+            ListNode next = cur.next;
+            for (int i = m; i < n; i++) {
+                next = next.next;
+            }
+            for (int i = m; i < n; i++) {
+                ListNode cur1 = cur.next;
+                cur.next = next;
+                next = cur;
+                cur = cur1;
+            }
+            cur.next = next;
+            pree.next = cur;
+            return pre.next;
+        }
+    }
+
+    //BM3 链表中的节点每k个一组翻转
+    public class Solution3 {
+        public class ListNode {
+            int val;
+            ListNode next = null;
+
+            public ListNode(int val) {
+                this.val = val;
+            }
+        }
+
+        /**
+         * 代码中的类名、方法名、参数名已经指定，请勿修改，直接返回方法规定的值即可
+         *
+         * @param head ListNode类
+         * @param k    int整型
+         * @return ListNode类
+         */
+        public ListNode reverseKGroup(ListNode head, int k) {
+            // write code here
+            ListNode cur=head;
+            int count=0;
+            while(cur!=null){
+                cur=cur.next;
+                count++;
+            }
             ListNode pre=new ListNode(0);
             pre.next=head;
-            ListNode cur=head;
-            ListNode pree=pre;
-            for(int i=1;i<m;i++){
-                pree=pree.next;
-                cur=cur.next;
+            ListNode p0=pre;
+            ListNode pp=null;
+            cur=head;
+            for(int i=count;i>=k;i-=k){
+                for(int j=0;j<k;j++){
+                    ListNode next = cur.next;
+                    cur.next=pp;
+                    pp=cur;
+                    cur=next;
+                }
+                ListNode next = p0.next;
+                p0.next.next=cur;
+                p0.next=pp;
+                p0=next;
             }
-            ListNode next=cur.next;
-            for (int i=m;i<n;i++){
-                next=next.next;
-            }
-            for(int i=m;i<n;i++){
-                ListNode cur1 = cur.next;
-                cur.next=next;
-                next=cur;
-                cur=cur1;
-            }
-            cur.next=next;
-            pree.next=cur;
             return pre.next;
         }
     }
