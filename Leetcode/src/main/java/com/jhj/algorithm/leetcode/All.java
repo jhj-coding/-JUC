de0cf776fc41ff8c32d0dc646a207ee38d296930
jhj <2539111904@qq.com>
2023-10-11 14:52:49 +0800

    3

--- Leetcode/src/main/java/com/jhj/algorithm/leetcode/All.java
+++ Leetcode/src/main/java/com/jhj/algorithm/leetcode/All.java
@@ -897,4 +897,20 @@ public class All {
             return j+1;
         }
     }
+
+    //27 移除元素
+    class Solution {
+        public int removeElement(int[] nums, int val) {
+            int j=0;
+            for(int i=0;i<nums.length;i++){
+                if(nums[i]==val){
+
+                }else {
+                    nums[j]=nums[i];
+                    j++;
+                }
+            }
+            return j;
+        }
+    }
 }
