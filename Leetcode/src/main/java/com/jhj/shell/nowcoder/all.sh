fd3a58c203cfab31c337d1c5c0e9a2630460ac46
jhj <2539111904@qq.com>
2023-10-11 19:42:23 +0800

    3

--- Leetcode/src/main/java/com/jhj/shell/nowcoder/all.sh
+++ Leetcode/src/main/java/com/jhj/shell/nowcoder/all.sh
@@ -8,3 +8,11 @@ wc -l nowcoder.txt | awk '{print $1}'
 #tail 显示尾部几行
 #sed 命令打印第几行 https://blog.csdn.net/Logicr/article/details/112845375
 tail -n 5 nowcoder.txt
+#SHELL3 输出 0 到 500 中 7 的倍数
+#空格不能少
+b=0;
+while [ $b -le 500 ]
+    do
+        echo $b
+        let b+=7;
+    done
