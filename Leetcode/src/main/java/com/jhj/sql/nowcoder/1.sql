61f4a3e2d7b39497b5c3d289d74df346d761f2b5
jhj <2539111904@qq.com>
2023-10-11 19:25:11 +0800

    3

--- Leetcode/src/main/java/com/jhj/sql/nowcoder/1.sql
+++ Leetcode/src/main/java/com/jhj/sql/nowcoder/1.sql
@@ -3,3 +3,6 @@ select * from user_profile;
 
 #SQL2 查询多列
 select device_id,gender,age,university from user_profile
+
+#SQL3 查询结果去重
+select university from user_profile group by university;
--- Leetcode/src/main/java/com/jhj/sql/nowcoder/all.sql
+++ Leetcode/src/main/java/com/jhj/sql/nowcoder/all.sql
@@ -2,3 +2,5 @@
 select * from employees order by hire_date desc limit 1;
 #SQL196 查找入职员工时间排名倒数第三的员工所有信息 distinct去重
 select * from employees where hire_date =(select distinct hire_date from employees order by hire_date desc limit 2,1);
+#SQL197 查找当前薪水详情以及部门编号dept_no
+select salaries.emp_no, salaries.salary, salaries.from_date, salaries.to_date, dept_manager.dept_no from salaries right join dept_manager on salaries.emp_no=dept_manager.emp_no order by salaries.emp_no;
