814dbc9db773c40e1105b131fa8496330468ef9a
jhj <2539111904@qq.com>
2023-10-13 16:28:17 +0800

    3

--- Leetcode/src/main/java/com/jhj/sql/nowcoder/1.sql
+++ Leetcode/src/main/java/com/jhj/sql/nowcoder/1.sql
@@ -6,3 +6,9 @@ select device_id,gender,age,university from user_profile
 
 #SQL3 查询结果去重
 select university from user_profile group by university;
+
+#SQL4 查询结果限制返回行数
+select device_id from user_profile limit 2;
+
+SQL5 将查询后的列重新命名
+select device_id as user_infos_example from user_profile limit 2;
