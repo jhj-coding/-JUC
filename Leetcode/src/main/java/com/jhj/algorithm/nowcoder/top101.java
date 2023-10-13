140053b7cb02a899397490fc86cf9db7ceb253dc
jhj <2539111904@qq.com>
2023-10-13 17:28:28 +0800

    3

--- Leetcode/src/main/java/com/jhj/algorithm/nowcoder/top101.java
+++ Leetcode/src/main/java/com/jhj/algorithm/nowcoder/top101.java
@@ -1,5 +1,9 @@
 package com.jhj.algorithm.nowcoder;
 
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.PriorityQueue;
+
 public class top101 {
 
     //BM1 反转链表
@@ -98,30 +102,184 @@ public class top101 {
          */
         public ListNode reverseKGroup(ListNode head, int k) {
             // write code here
-            ListNode cur=head;
-            int count=0;
-            while(cur!=null){
-                cur=cur.next;
+            ListNode cur = head;
+            int count = 0;
+            while (cur != null) {
+                cur = cur.next;
                 count++;
             }
-            ListNode pre=new ListNode(0);
-            pre.next=head;
-            ListNode p0=pre;
-            ListNode pp=null;
-            cur=head;
-            for(int i=count;i>=k;i-=k){
-                for(int j=0;j<k;j++){
+            ListNode pre = new ListNode(0);
+            pre.next = head;
+            ListNode p0 = pre;
+            ListNode pp = null;
+            cur = head;
+            for (int i = count; i >= k; i -= k) {
+                for (int j = 0; j < k; j++) {
                     ListNode next = cur.next;
-                    cur.next=pp;
-                    pp=cur;
-                    cur=next;
+                    cur.next = pp;
+                    pp = cur;
+                    cur = next;
                 }
                 ListNode next = p0.next;
-                p0.next.next=cur;
-                p0.next=pp;
-                p0=next;
+                p0.next.next = cur;
+                p0.next = pp;
+                p0 = next;
             }
             return pre.next;
+        }
+    }
+
+    //BM4 合并两个排序的链表
+    public class Solution4 {
+        public class ListNode {
+            int val;
+            ListNode next = null;
+
+            public ListNode(int val) {
+                this.val = val;
+            }
+        }
+
+        /**
+         * 代码中的类名、方法名、参数名已经指定，请勿修改，直接返回方法规定的值即可
+         *
+         * @param pHead1 ListNode类
+         * @param pHead2 ListNode类
+         * @return ListNode类
+         */
+        public ListNode Merge(ListNode pHead1, ListNode pHead2) {
+            // write code here
+            ListNode cur1 = pHead1;
+            ListNode cur2 = pHead2;
+            ListNode head = new ListNode(0);
+            ListNode cur3 = head;
+            while (cur1 != null || cur2 != null) {
+                if (cur1 == null) {
+                    cur3.next = cur2;
+                    break;
+                } else if (cur2 == null) {
+                    cur3.next = cur1;
+                    break;
+                } else {
+                    if (cur1.val < cur2.val) {
+                        cur3.next = new ListNode(cur1.val);
+                        cur1 = cur1.next;
+                    } else {
+                        cur3.next = new ListNode(cur2.val);
+                        cur2 = cur2.next;
+                    }
+                    cur3 = cur3.next;
+                }
+            }
+            return head.next;
+        }
+    }
+
+    public class Solution {
+        public class ListNode {
+            int val;
+            ListNode next = null;
+
+            public ListNode(int val) {
+                this.val = val;
+            }
+        }
+
+        /**
+         * 代码中的类名、方法名、参数名已经指定，请勿修改，直接返回方法规定的值即可
+         *
+         * @param lists ListNode类ArrayList
+         * @return ListNode类
+         */
+        public ListNode mergeKLists1(ArrayList<ListNode> lists) {
+            ArrayList<Integer> integers = new ArrayList<>();
+            // write code here
+            for (int i = 0; i < lists.size(); i++) {
+                ListNode listNode = lists.get(i);
+                while (listNode != null) {
+                    integers.add(listNode.val);
+                    listNode = listNode.next;
+                }
+            }
+            integers.sort(new Comparator<Integer>() {
+                @Override
+                public int compare(Integer o1, Integer o2) {
+                    return o1 - o2;
+                }
+            });
+            ListNode head = new ListNode(0);
+            ListNode cur = head;
+            for (int i = 0; i < integers.size(); i++) {
+                cur.next = new ListNode(integers.get(i));
+                cur = cur.next;
+            }
+            return head.next;
+        }
+
+        public ListNode mergeKLists2(ArrayList<ListNode> lists) {
+            // write code here
+            PriorityQueue<ListNode> objects = new PriorityQueue<ListNode>(new Comparator<ListNode>() {
+                @Override
+                public int compare(ListNode o1, ListNode o2) {
+                    return o1.val - o2.val;
+                }
+            });
+
+            for (ListNode cur : lists) {
+                if (cur != null) objects.add(cur);
+            }
+            ListNode head = new ListNode(0);
+            ListNode curr = head;
+            while (!objects.isEmpty()) {
+                ListNode remove = objects.remove();
+                curr.next = remove;
+                curr = curr.next;
+                if (remove.next != null)
+                    objects.add(remove.next);
+            }
+            return head.next;
+        }
+
+        public ListNode mergeKLists(ArrayList<ListNode> lists) {
+            // write code here
+            return merge(lists, 0, lists.size() - 1);
+        }
+
+        public ListNode merge(ArrayList<ListNode> lists, int l, int right) {
+            if (l == right) {
+                return lists.get(l);
+            }
+            if (l > right) {
+                return null;
+            }
+            int mid = l + (right - l) / 2;
+            return mergeTwoList(merge(lists, l, mid), merge(lists, mid + 1, right));
+        }
+
+        public ListNode mergeTwoList(ListNode l1, ListNode l2) {
+            ListNode cur1 = l1;
+            ListNode cur2 = l2;
+            ListNode head = new ListNode(0);
+            ListNode cur3 = head;
+            while (cur1 != null || cur2 != null) {
+                if (cur1 == null) {
+                    cur3.next = cur2;
+                    break;
+                } else if (cur2 == null) {
+                    cur3.next = cur1;
+                    break;
+                } else {
+                    if (cur1.val < cur2.val) {
+                        cur3.next = cur1;
+                        cur1 = cur1.next;
+                    } else {
+                        cur3.next = cur2;
+                        cur2 = cur2.next;
+                    }
+                    cur3 = cur3.next;
+                }
+            }
+            return head.next;
         }
     }
 }
