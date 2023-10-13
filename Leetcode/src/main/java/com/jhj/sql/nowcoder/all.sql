efdb670fde9abe451f5cb5da19a48ce44cd54795
jhj <2539111904@qq.com>
2023-10-13 16:31:30 +0800

    3

--- Leetcode/src/main/java/com/jhj/sql/nowcoder/all.sql
+++ Leetcode/src/main/java/com/jhj/sql/nowcoder/all.sql
@@ -4,3 +4,7 @@ select * from employees order by hire_date desc limit 1;
 select * from employees where hire_date =(select distinct hire_date from employees order by hire_date desc limit 2,1);
 #SQL197 查找当前薪水详情以及部门编号dept_no
 select salaries.emp_no, salaries.salary, salaries.from_date, salaries.to_date, dept_manager.dept_no from salaries right join dept_manager on salaries.emp_no=dept_manager.emp_no order by salaries.emp_no;
+#SQL198 查找所有已经分配部门的员工的last_name和first_name以及dept_no
+select employees.last_name,employees.first_name,dept_emp.dept_no   from employees,dept_emp where employees.emp_no = dept_emp.emp_no;
+#SQL199 查找所有员工的last_name和first_name以及对应部门编号dept_no
+select employees.last_name,employees.first_name,dept_emp.dept_no  from employees left join dept_emp on employees.emp_no =dept_emp.emp_no
