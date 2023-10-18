93a8a9808e48f3ed42e659ad7439c6ac559b0a87
jhj <2539111904@qq.com>
2023-10-18 16:53:15 +0800

    3

--- Leetcode/src/main/java/com/jhj/algorithm/nowcoder/all.java
+++ Leetcode/src/main/java/com/jhj/algorithm/nowcoder/all.java
@@ -1,6 +1,8 @@
 package com.jhj.algorithm.nowcoder;
 
 import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.LinkedList;
 
 public class all {
 
@@ -249,6 +251,71 @@ public class all {
             return root.val + Math.max(left, right);
         }
 
+    }
+
+    //NC7 买卖股票的最好时机(一)
+    public class Solution7 {
+        /**
+         * 代码中的类名、方法名、参数名已经指定，请勿修改，直接返回方法规定的值即可
+         *
+         * @param prices int整型一维数组
+         * @return int整型
+         */
+        public int maxProfit(int[] prices) {
+            // write code here
+            int min = prices[0];
+            int res = 0;
+            for (int i = 1; i < prices.length; i++) {
+                min = Math.min(min, prices[i]);
+                res = Math.max(res, prices[i] - min);
+            }
+            return res;
+        }
+    }
+
+    //NC8 二叉树中和为某一值的路径(二)
+    public class Solution8 {
+        public class TreeNode {
+            int val = 0;
+            TreeNode left = null;
+            TreeNode right = null;
+
+            public TreeNode(int val) {
+                this.val = val;
+            }
+        }
+
+        /**
+         * 代码中的类名、方法名、参数名已经指定，请勿修改，直接返回方法规定的值即可
+         *
+         * @param root   TreeNode类
+         * @param target int整型
+         * @return int整型ArrayList<ArrayList <>>
+         */
+        private ArrayList<ArrayList<Integer>> ret = new ArrayList<>();
+        private LinkedList<Integer> path = new LinkedList<>();
+
+        void dfs(TreeNode root, int number) {
+            // 处理树为空
+            if (root == null) return;
+            // 路径更新
+            path.add(root.val);
+            // number更新
+            number -= root.val;
+            // 如果递归当前节点为叶子节点且该条路径的值已经达到了expectNumber，则更新ret
+            if(root.left == null && root.right == null && number == 0) {
+                ret.add(new ArrayList<>(path));
+            }
+            // 左右子树递归
+            dfs(root.left, number);
+            dfs(root.right, number);
+            path.removeLast();
+        }
+
+        public ArrayList<ArrayList<Integer>> FindPath(TreeNode root,int expectNumber) {
+            dfs(root, expectNumber);
+            return ret;
+        }
     }
 
 }
