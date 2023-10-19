0eb370040e895351319e5b5f42c9e20581034743
jhj <2539111904@qq.com>
2023-10-19 21:36:09 +0800

    3

---
+++ Leetcode/src/main/java/com/jhj/algorithm/nowcoder/Main.java
@@ -0,0 +1,22 @@
+package com.jhj.algorithm.nowcoder;
+
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.StreamTokenizer;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader ins = new BufferedReader(new InputStreamReader(System.in));
+        StreamTokenizer in = new StreamTokenizer(ins);
+        in.nextToken();
+        int n = (int)in.nval;
+        in.nextToken();
+        int k = (int)in.nval;
+
+
+    }
+
+}
