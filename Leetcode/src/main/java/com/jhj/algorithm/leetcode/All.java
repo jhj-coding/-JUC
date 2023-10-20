51af18a73400b6f5869b0c064f260e7fa72ae2c0
jhj <2539111904@qq.com>
2023-10-20 15:51:35 +0800

    3

--- Leetcode/src/main/java/com/jhj/algorithm/leetcode/All.java
+++ Leetcode/src/main/java/com/jhj/algorithm/leetcode/All.java
@@ -1066,4 +1066,32 @@ public class All {
             return res;
         }
     }
+
+    //33. 搜索旋转排序数组
+    class Solution33 {
+        public int search(int[] nums, int target) {
+            int left=0;
+            int right=nums.length-1;
+            while (left<=right){
+                int mid=(left+right)/2;
+                if(nums[mid]==target){
+                    return mid;
+                }
+                if(nums[0]<=nums[mid]){
+                    if(nums[0]<=target&&target<nums[mid]){
+                        right=mid-1;
+                    }else {
+                        left=mid+1;
+                    }
+                }else{
+                    if(target>nums[mid]&&target<=nums[nums.length-1]){
+                        left=mid+1;
+                    }else{
+                        right=mid-1;
+                    }
+                }
+            }
+            return -1;
+        }
+    }
 }
