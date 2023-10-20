109375b72450738e2ea49ef1f4840d022bdbd351
jhj <2539111904@qq.com>
2023-10-20 19:07:37 +0800

    3

--- Leetcode/src/main/java/com/jhj/algorithm/nowcoder/Main.java
+++ Leetcode/src/main/java/com/jhj/algorithm/nowcoder/Main.java
@@ -5,6 +5,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.StreamTokenizer;
+import java.math.BigDecimal;
 
 public class Main {
 
@@ -12,9 +13,19 @@ public class Main {
         BufferedReader ins = new BufferedReader(new InputStreamReader(System.in));
         StreamTokenizer in = new StreamTokenizer(ins);
         in.nextToken();
-        int n = (int)in.nval;
-        in.nextToken();
-        int k = (int)in.nval;
+        int t = (int)in.nval;
+        for(int i=0;i<t;i++) {
+            in.nextToken();
+            int y = (int) in.nval;
+            in.nextToken();
+            int z = (int) in.nval;
+            in.nextToken();
+            int b = (int) in.nval;
+            double v = Math.pow(y, Math.E) / (z * 1.0);
+            BigDecimal bigDecimal = new BigDecimal(v);
+            System.out.println(bigDecimal.setScale(b, BigDecimal.ROUND_HALF_EVEN));
+        }
+
 
 
     }
