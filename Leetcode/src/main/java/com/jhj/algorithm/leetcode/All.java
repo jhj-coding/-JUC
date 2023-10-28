be57b6d9d5219a268082687667657c46a0a9c3c1
jhj <2539111904@qq.com>
2023-10-28 22:26:03 +0800

    3

--- Leetcode/src/main/java/com/jhj/algorithm/leetcode/All.java
+++ Leetcode/src/main/java/com/jhj/algorithm/leetcode/All.java
@@ -1258,5 +1258,31 @@ public class All {
 
     }
 
-
+    //39. 组合总和
+    class Solution39 {
+        public List<List<Integer>> combinationSum(int[] candidates, int target) {
+            List<List<Integer>> res=new ArrayList<List<Integer>>();
+            int length=candidates.length;
+            if(length<=0){
+                return res;
+            }
+            ArrayList<Integer> integers = new ArrayList<>();
+            dfs(candidates,0,length,integers,res,target);
+            return res;
+        }
+        public void dfs(int[] candidates,int begin,int end,List<Integer> path,List<List<Integer>> res,int target){
+            if(target<0){
+                return;
+            }
+            if(target==0){
+                res.add(new ArrayList<>(path));
+                return;
+            }
+            for(int i=begin;i<end;i++){
+                path.add(candidates[i]);
+                dfs(candidates,i,end,path,res,target-candidates[i]);
+                path.remove(path.size()-1);
+            }
+        }
+    }
 }
