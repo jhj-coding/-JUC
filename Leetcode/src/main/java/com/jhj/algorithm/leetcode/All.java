228362370128f4c5b3cea8922155f4ee44626da1
jhj <2539111904@qq.com>
2023-11-02 15:32:40 +0800

    3

--- Leetcode/src/main/java/com/jhj/algorithm/leetcode/All.java
+++ Leetcode/src/main/java/com/jhj/algorithm/leetcode/All.java
@@ -1388,4 +1388,42 @@ public class All {
             return res;
         }
     }
+
+    //43. 字符串相乘 注意分完之后 数组0 表示最高位
+    class Solution43 {
+        public String multiply(String num1, String num2) {
+            int length1 = num1.length();
+            int length2 = num2.length();
+            int[] ints1 = new int[length1];
+            int[] ints2 = new int[length2];
+            for(int i=0;i<length1;i++){
+                ints1[i]=num1.charAt(i)-'0';
+            }
+            for(int i=0;i<length2;i++){
+                ints2[i]=num2.charAt(i)-'0';
+            }
+            int[] ints = new int[length1 + length2];
+            for(int i=0;i<length1;i++){
+                for (int j=0;j<length2;j++){
+                    ints[i+j+1]+=ints1[i]*ints2[j];
+                }
+            }
+            int m=0;
+            for(int i=length1 + length2-1;i>=0;i--){
+                ints[i]+=m;
+                m=ints[i]/10;
+                ints[i]%=10;
+                System.out.println(ints[i]);
+            }
+            int j=0;
+            while (j<length1+length2&&ints[j]==0){
+                j++;
+            }
+            StringBuilder res = new StringBuilder();
+            for(int i=j;i<length1+length2;i++){
+                res.append(ints[i]);
+            }
+            return res.length()==0?"0":res.toString();
+        }
+    }
 }
