8e094d21000247bafaba2dd0b7dce58b0da8df49
jhj <2539111904@qq.com>
2023-11-19 21:15:43 +0800

    3

--- Leetcode/src/main/java/com/jhj/algorithm/leetcode/All.java
+++ Leetcode/src/main/java/com/jhj/algorithm/leetcode/All.java
@@ -1539,4 +1539,29 @@ public class All {
             }
         }
     }
+
+    //旋转图像
+    class Solution48 {
+        public void rotate(int[][] matrix) {
+            int n=matrix.length;
+            for(int i=0;i<n;i++){
+                for(int j=0;j<i;j++){
+                    int temp=matrix[i][j];
+                    matrix[i][j]=matrix[j][i];
+                    matrix[j][i]=temp;
+                }
+            }
+            for(int i=0;i<n;i++){
+                int j=0;
+                int k=n-1;
+                while (j<k){
+                    int temp=matrix[i][j];
+                    matrix[i][j]=matrix[i][k];
+                    matrix[i][k]=temp;
+                    j++;
+                    k--;
+                }
+            }
+        }
+    }
 }
