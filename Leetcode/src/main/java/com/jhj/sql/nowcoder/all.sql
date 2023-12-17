381c6eb262b0cbc6de97bdf7dde7c03ac033a9bc
jhj <2539111904@qq.com>
2023-12-17 22:02:26 +0800

    3

--- Leetcode/src/main/java/com/jhj/sql/nowcoder/all.sql
+++ Leetcode/src/main/java/com/jhj/sql/nowcoder/all.sql
@@ -233,3 +233,11 @@ WHERE
     )
 ORDER BY
     u_n;
+
+#SQL262 牛客每个人最近的登录日期(三)
+SELECT
+    ROUND(COUNT(DISTINCT user_id)*1.0 / (SELECT COUNT(DISTINCT user_id) FROM login), 3)
+FROM login
+WHERE (user_id, date)
+          IN
+      (SELECT user_id, DATE_ADD(MIN(date),INTERVAL 1 DAY) FROM login GROUP BY user_id);
