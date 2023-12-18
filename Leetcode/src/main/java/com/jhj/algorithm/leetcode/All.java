711cb4e6a314335a948f0979b9224b2cf3c9278a
jhj <2539111904@qq.com>
2023-12-18 21:56:05 +0800

    3

--- Leetcode/src/main/java/com/jhj/algorithm/leetcode/All.java
+++ Leetcode/src/main/java/com/jhj/algorithm/leetcode/All.java
@@ -2135,4 +2135,51 @@ public class All {
             return f[m-1][n-1];
         }
     }
+
+    //63. 不同路径 II
+    class Solution63 {
+        public int uniquePathsWithObstacles(int[][] obstacleGrid) {
+            int m = obstacleGrid.length;
+            int n = obstacleGrid[0].length;
+            int[][] f = new int[m][n];
+            if(obstacleGrid[0][0]==1 || obstacleGrid[m-1][n-1]==1 ){
+                return 0;
+            }
+            f[0][0]=1;
+            for(int i=1;i<n;i++){
+                if(obstacleGrid[0][i]==1){
+                    f[0][i]=0;
+                }else {
+                    f[0][i] = f[0][i - 1];
+                    if(f[0][i]==0){
+                        obstacleGrid[0][i]=1;
+                    }
+                }
+            }
+            for(int i=1;i<m;i++){
+                if(obstacleGrid[i][0]==1){
+                    f[i][0]=0;
+                }else {
+                    f[i][0] = f[i-1][0];
+                    if(f[i][0]==0){
+                        obstacleGrid[i][0]=1;
+                    }
+                }
+            }
+            for(int i=1;i<m;i++){
+                for(int j=1;j<n;j++){
+                    if(obstacleGrid[i][j-1]==1&&obstacleGrid[i-1][j]==1){
+                        continue;
+                    }else if(obstacleGrid[i][j-1]==1){
+                        f[i][j]=f[i-1][j];
+                    }else if(obstacleGrid[i-1][j]==1){
+                        f[i][j]=f[i][j-1];
+                    }else{
+                        f[i][j]=f[i-1][j]+f[i][j-1];
+                    }
+                }
+            }
+            return f[m-1][n-1];
+        }
+    }
 }
