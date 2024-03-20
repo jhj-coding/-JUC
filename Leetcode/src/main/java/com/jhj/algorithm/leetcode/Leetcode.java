c4990abb9abeb10dcba58551490ac70335e48f3f
jhj <hnjzjhj@163.com>
2024-03-20 15:24:20 +0800

    leetcode 129

--- Leetcode/src/main/java/com/jhj/algorithm/leetcode/Leetcode.java
+++ Leetcode/src/main/java/com/jhj/algorithm/leetcode/Leetcode.java
@@ -1,54 +1,43 @@
 package com.jhj.algorithm.leetcode;
 
 
-import java.util.Arrays;
-
 public class Leetcode {
-    public class Solution {
-        /**
-         * 代码中的类名、方法名、参数名已经指定，请勿修改，直接返回方法规定的值即可
-         *
-         *
-         * @param s string字符串
-         * @param k int整型
-         * @return int整型
-         */
-        public int numKLenSubstrRepeats (String s, int k) {
-            // write code here
-            int[] a = new int[26];
-            Arrays.fill(a,0);
-            if(k>s.length()){
-                return 0;
-            }
-            int i=0;
-            int j=k;
-            for(int l=i;l<j;l++){
-                a[s.charAt(l)-'a']++;
-            }
-            int res=0;
-            if(panduan(a)){
-                res++;
-            }
-            while (j<s.length()){
-                a[s.charAt(i)-'a']--;
-                a[s.charAt(j)-'a']++;
-                if(panduan(a)){
-                    res++;
-                }
-                i++;
-                j++;
-            }
+    public class TreeNode {
+      int val;
+      TreeNode left;
+      TreeNode right;
+      TreeNode() {}
+      TreeNode(int val) { this.val = val; }
+      TreeNode(int val, TreeNode left, TreeNode right) {
+          this.val = val;
+          this.left = left;
+          this.right = right;
+      }
+  }
+    class Solution {
+        int res = 0;
+        int path = 0;
+
+        public int sumNumbers(TreeNode root) {
+            dfs(root);
             return res;
         }
 
-        public boolean panduan(int a[]){
-
-            for(int i=0;i<26;i++){
-                if(a[i]>=2){
-                    return true;
-                }
+        public void dfs(TreeNode root) {
+            if (root == null) {
+                return;
             }
-            return false;
+            if (root.left == null && root.right == null) {
+                System.out.println(path*10+root.val);
+                res += path*10+root.val;
+                return;
+            }
+            path = path*10 + root.val;
+            if(root.left!=null)
+                dfs(root.left);
+            if(root.right!=null)
+                dfs(root.right);
+            path/=10;
         }
     }
 }
