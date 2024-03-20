1c32abc47e1317d635b31db2f2c6598e228b5c00
jhj <hnjzjhj@163.com>
2024-03-20 16:31:07 +0800

    nc230

--- Leetcode/src/main/java/com/jhj/algorithm/leetcode/Leetcode.java
+++ Leetcode/src/main/java/com/jhj/algorithm/leetcode/Leetcode.java
@@ -1,10 +1,6 @@
 package com.jhj.algorithm.leetcode;
 
 
-import java.util.Comparator;
-import java.util.Map;
-import java.util.TreeMap;
-
 public class Leetcode {
 
 //     * Definition for a binary tree node.
@@ -21,34 +17,17 @@ public class Leetcode {
           }
       }
 
-    class Solution {
-        public String[] findRelativeRanks(int[] score) {
-            String[] strings = new String[score.length];
-            TreeMap<Integer, Integer> map = new TreeMap<>(new Comparator<Integer>() {
-                @Override
-                public int compare(Integer o1, Integer o2) {
-                    return o2-o1;
-                }
-            });
-            for(int i=0;i<score.length;i++) {
-                map.put(score[i],i);
-            }
-            int i=1;
-            for(Map.Entry<Integer,Integer> e:map.entrySet()) {
-                if(i==1) {
-                    strings[e.getValue()] = String.valueOf("Gold Medal");
-                }else if(i==2) {
-                    strings[e.getValue()] = String.valueOf("Bronze Medal");
-
-                }else if(i==3) {
-                    strings[e.getValue()] = String.valueOf("Silver Medal");
-
-                }else {
-                    strings[e.getValue()] = String.valueOf(i);
-                }
-                i++;
-            }
-            return strings;
+    public class Solution {
+        /**
+         * 代码中的类名、方法名、参数名已经指定，请勿修改，直接返回方法规定的值即可
+         *
+         *
+         * @param n int整型 
+         * @return bool布尔型
+         */
+        public boolean NimGame (int n) {
+            // write code here
+            return n%4!=0;
         }
     }
 
