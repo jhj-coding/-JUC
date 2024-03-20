ed1f49a48e6dcc91a8b005868c37f83b14cfca9a
jhj <hnjzjhj@163.com>
2024-03-20 16:00:49 +0800

    leetcode 222

--- Leetcode/src/main/java/com/jhj/algorithm/leetcode/Leetcode.java
+++ Leetcode/src/main/java/com/jhj/algorithm/leetcode/Leetcode.java
@@ -1,35 +1,45 @@
 package com.jhj.algorithm.leetcode;
 
 
-public class Leetcode {
-    public class TreeNode {
-      int val;
-      TreeNode left;
-      TreeNode right;
-      TreeNode() {}
-      TreeNode(int val) { this.val = val; }
-      TreeNode(int val, TreeNode left, TreeNode right) {
-          this.val = val;
-          this.left = left;
-          this.right = right;
-      }
-  }
-    class Solution {
-        int max=Integer.MIN_VALUE;
-        public int maxPathSum(TreeNode root) {
-            dfs(root);
-            return max;
-        }
-        public int dfs(TreeNode root){
-            if(root==null){
-                return 0;
-            }
+import java.util.ArrayList;
 
-            int left = Math.max(dfs(root.left), 0);
-            int right = Math.max(dfs(root.right), 0);
-            int i = root.val + left + right;
-            max=Math.max(max,i);
-            return root.val+Math.max(left,right);
+public class Leetcode {
+
+//     * Definition for a binary tree node.
+      public class TreeNode {
+          int val;
+          TreeNode left;
+          TreeNode right;
+          TreeNode() {}
+          TreeNode(int val) { this.val = val; }
+          TreeNode(int val, TreeNode left, TreeNode right) {
+              this.val = val;
+              this.left = left;
+              this.right = right;
+          }
+      }
+
+    class Solution {
+          ArrayList<Integer> list=new ArrayList<Integer>();
+        public int countNodes(TreeNode root) {
+
+            dfs(root);
+            return list.size();
+        }
+        public void dfs(TreeNode root){
+            if(root==null){
+                return;
+            }
+            list.add(root.val);
+            dfs(root.left);
+            dfs(root.right);
         }
     }
+
+/**
+ * Your BSTIterator object will be instantiated and called as such:
+ * BSTIterator obj = new BSTIterator(root);
+ * int param_1 = obj.next();
+ * boolean param_2 = obj.hasNext();
+ */
 }
