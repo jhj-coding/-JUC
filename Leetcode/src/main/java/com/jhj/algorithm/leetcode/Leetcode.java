26c6a8fd9686f56b65da067206e2559b68897cf8
jhj <hnjzjhj@163.com>
2024-03-21 15:51:32 +0800

    leetcode 637

--- Leetcode/src/main/java/com/jhj/algorithm/leetcode/Leetcode.java
+++ Leetcode/src/main/java/com/jhj/algorithm/leetcode/Leetcode.java
@@ -14,8 +14,8 @@ public class Leetcode {
   }
 
     class Solution {
-        public List<Integer> rightSideView(TreeNode root) {
-            ArrayList<Integer> res = new ArrayList<>();
+        public List<Double> averageOfLevels(TreeNode root) {
+            ArrayList<Double> res = new ArrayList<>();
             if(root==null){
                 return res;
             }
@@ -23,19 +23,19 @@ public class Leetcode {
             deque.addLast(root);
             while (!deque.isEmpty()){
                 int size = deque.size();
+                long sum=0;
+                int n=size;
                 while (size-->0){
                     TreeNode treeNode = deque.removeFirst();
-
                     if(treeNode.left!=null){
                         deque.addLast(treeNode.left);
                     }
                     if(treeNode.right!=null){
                         deque.addLast(treeNode.right);
                     }
-                    if(size==0){
-                        res.add(treeNode.val);
-                    }
+                    sum+=treeNode.val;
                 }
+                res.add(sum*1.0/n);
             }
             return res;
         }
