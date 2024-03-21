f5650352ac0f9a1fbb85fe16ed3bc9f9480903f5
jhj <hnjzjhj@163.com>
2024-03-21 19:33:08 +0800

    nb 10

--- Leetcode/src/main/java/com/jhj/algorithm/leetcode/Leetcode.java
+++ Leetcode/src/main/java/com/jhj/algorithm/leetcode/Leetcode.java
@@ -1,9 +1,7 @@
 package com.jhj.algorithm.leetcode;
 
 
-import java.util.Arrays;
-import java.util.Deque;
-import java.util.LinkedList;
+import java.util.*;
 
 public class Leetcode {
     public class TreeNode {
@@ -13,19 +11,35 @@ public class Leetcode {
       TreeNode(int x) { val = x; }
   }
 
-    class Solution {
-        public int[] nextGreaterElements(int[] nums) {
-            int n = nums.length;
-            int[] ret = new int[n];
-            Arrays.fill(ret, -1);
-            Deque<Integer> stack = new LinkedList<Integer>();
-            for (int i = 0; i < n * 2 - 1; i++) {
-                while (!stack.isEmpty() && nums[stack.peek()] < nums[i % n]) {
-                    ret[stack.pop()] = nums[i % n];
+    public class Solution {
+        /**
+         * 代码中的类名、方法名、参数名已经指定，请勿修改，直接返回方法规定的值即可
+         *
+         *
+         * @param nums int整型一维数组 
+         * @param k int整型 
+         * @return int整型一维数组
+         */
+        public int[] minSlidingWindow (int[] nums, int k) {
+            // write code here
+            ArrayDeque<Integer> deque = new ArrayDeque<>();
+            int n=nums.length;
+            int[] res=new int[n-k+1];
+            for(int i=0;i<n;i++){
+                while (!deque.isEmpty()&&deque.peekFirst()<i-k+1){
+                    deque.pollFirst();
                 }
-                stack.push(i % n);
+
+                while (!deque.isEmpty()&&nums[deque.peekLast()]>nums[i]){
+                    deque.pollLast();
+                }
+                deque.offerLast(i);
+                if(i>=k-1){
+                    res[i-k+1]=nums[deque.peekFirst()];
+                }
             }
-            return ret;
+
+            return res;
         }
     }
 }
