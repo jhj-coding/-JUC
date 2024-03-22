00b477983a975a12d7bc74a4258d1e417069b969
jhj <hnjzjhj@163.com>
2024-03-22 14:11:51 +0800

    leetcode 530

--- Leetcode/src/main/java/com/jhj/algorithm/leetcode/Leetcode.java
+++ Leetcode/src/main/java/com/jhj/algorithm/leetcode/Leetcode.java
@@ -1,8 +1,6 @@
 package com.jhj.algorithm.leetcode;
 
 
-import java.util.*;
-
 public class Leetcode {
     public class TreeNode {
       int val;
@@ -11,35 +9,26 @@ public class Leetcode {
       TreeNode(int x) { val = x; }
   }
 
-    public class Solution {
-        /**
-         * 代码中的类名、方法名、参数名已经指定，请勿修改，直接返回方法规定的值即可
-         *
-         *
-         * @param nums int整型一维数组 
-         * @param k int整型 
-         * @return int整型一维数组
-         */
-        public int[] minSlidingWindow (int[] nums, int k) {
-            // write code here
-            ArrayDeque<Integer> deque = new ArrayDeque<>();
-            int n=nums.length;
-            int[] res=new int[n-k+1];
-            for(int i=0;i<n;i++){
-                while (!deque.isEmpty()&&deque.peekFirst()<i-k+1){
-                    deque.pollFirst();
-                }
+    class Solution {
 
-                while (!deque.isEmpty()&&nums[deque.peekLast()]>nums[i]){
-                    deque.pollLast();
-                }
-                deque.offerLast(i);
-                if(i>=k-1){
-                    res[i-k+1]=nums[deque.peekFirst()];
-                }
-            }
-
+        int res=Integer.MAX_VALUE;
+        TreeNode pre=null;
+        public int getMinimumDifference(TreeNode root) {
+            dfs(root);
             return res;
         }
+
+        public void dfs(TreeNode root){
+            if(root==null){
+                return;
+            }
+            dfs(root.left);
+            if(pre!=null){
+                res=Math.min(root.val-pre.val,res);
+            }
+            pre=root;
+            dfs(root.right);
+        }
+
     }
 }
