505b457bee448dbfd266a2d667aecf388a566a9d
jhj <hnjzjhj@163.com>
2024-03-22 15:53:01 +0800

    nb 11

--- Leetcode/src/main/java/com/jhj/algorithm/leetcode/Leetcode.java
+++ Leetcode/src/main/java/com/jhj/algorithm/leetcode/Leetcode.java
@@ -1,9 +1,6 @@
 package com.jhj.algorithm.leetcode;
 
 
-import java.util.Arrays;
-import java.util.PriorityQueue;
-
 public class Leetcode {
     public class TreeNode {
       int val;
@@ -12,28 +9,45 @@ public class Leetcode {
       TreeNode(int x) { val = x; }
   }
 
-    class Solution {
-        public int findMaximizedCapital(int k, int w, int[] profits, int[] capital) {
-            int n=profits.length;
-            int[][] arr = new int[n][2];
-            for(int i=0;i<profits.length;i++){
-                arr[i]=new int[]{capital[i],profits[i]};
+    public class Solution {
+        /**
+         * 代码中的类名、方法名、参数名已经指定，请勿修改，直接返回方法规定的值即可
+         *
+         *
+         * @param s string字符串
+         * @param t string字符串
+         * @return string字符串
+         */
+        public String minWindow (String s, String t) {
+            // write code here
+            int[] hash = new int[128];
+            for(int i=0;i<t.length();i++){
+                hash[t.charAt(i)]--;
             }
-            Arrays.sort(arr,(a,b)->a[0]-b[0]);
-            PriorityQueue<Integer> priorityQueue = new PriorityQueue<>((x, y) -> y - x);
-            int curr=0;
-            for(int i=0;i<k;i++){
-                while (curr<n&&arr[curr][0]<=w){
-                    priorityQueue.add(arr[curr][1]);
-                    curr++;
+            int left =0;
+            int right=0;
+            boolean isFind=false;
+            String res=s;
+            while (right<s.length()){
+                hash[s.charAt(right)]++;
+                while (check(hash)){
+                    isFind=true;
+                    if(res.length() > right - left + 1){
+                        res = s.substring(left , right + 1);
+                    }
+                    hash[s.charAt(left)]--;
+                    left++;
                 }
-                if(!priorityQueue.isEmpty()){
-                     w+= priorityQueue.poll();
-                }else{
-                    break;
-                }
+                right++;
             }
-            return w;
+            return isFind ? res : "";
+        }
+        boolean check(int[] hash){
+            for (int i : hash) {
+                if(i < 0)
+                    return false;
+            }
+            return true;
         }
     }
 }
