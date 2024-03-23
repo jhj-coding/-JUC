fd4683fe7e1f8f06737f751d06c571990344974a
jhj <hnjzjhj@163.com>
2024-03-23 11:07:16 +0800

    nb 133

--- Leetcode/src/main/java/com/jhj/algorithm/leetcode/Leetcode.java
+++ Leetcode/src/main/java/com/jhj/algorithm/leetcode/Leetcode.java
@@ -1,55 +1,43 @@
 package com.jhj.algorithm.leetcode;
 
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
 public class Leetcode {
-    public class TreeNode {
-      int val;
-      TreeNode left;
-      TreeNode right;
-      TreeNode(int x) { val = x; }
-  }
-
-    public class Solution {
-        /**
-         * 代码中的类名、方法名、参数名已经指定，请勿修改，直接返回方法规定的值即可
-         *
-         *
-         * @param s string字符串
-         * @param t string字符串
-         * @return string字符串
-         */
-        public String minWindow (String s, String t) {
-            // write code here
-            int[] ts = new int[128];
-            for(int i=0;i<t.length();i++){
-                ts[t.charAt(i)]++;
-            }
-            int left=0;
-            int right=0;
-            String res=s;
-            boolean flag=false;
-            while (right<s.length()){
-                char c = s.charAt(right);
-                ts[c]--;
-                while (check(ts)){
-                    flag=true;
-                    res=res.length()>right-left+1?s.substring(left,right+1):res;
-                    char c1 = s.charAt(left);
-                    ts[c1]++;
-                    left++;
-                }
-                right++;
-            }
-            return flag?res:"";
+    class Node {
+        public int val;
+        public List<Node> neighbors;
+        public Node() {
+            val = 0;
+            neighbors = new ArrayList<Node>();
         }
+        public Node(int _val) {
+            val = _val;
+            neighbors = new ArrayList<Node>();
+        }
+        public Node(int _val, ArrayList<Node> _neighbors) {
+            val = _val;
+            neighbors = _neighbors;
+        }
+    }
 
-        private boolean check(int[] ts) {
-            for(int i=0;i<ts.length;i++){
-                if(ts[i]>0){
-                    return false;
-                }
+    class Solution {
+        HashMap<Node, Node> map = new HashMap<>();
+        public Node cloneGraph(Node node) {
+            if(node==null){
+                return node;
             }
-            return true;
+            if (map.containsKey(node)){
+                return map.get(node);
+            }
+            Node node1 = new Node(node.val, new ArrayList<>());
+            map.put(node,node1);
+            for(Node n:node.neighbors){
+                node1.neighbors.add(cloneGraph(n));
+            }
+            return node1;
         }
     }
 }
