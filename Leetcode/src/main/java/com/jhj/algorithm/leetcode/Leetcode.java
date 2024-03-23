c13af0347c4296c27da26ffe93f0b31a2ce08221
jhj <hnjzjhj@163.com>
2024-03-23 13:20:12 +0800

    leetcode 77

--- Leetcode/src/main/java/com/jhj/algorithm/leetcode/Leetcode.java
+++ Leetcode/src/main/java/com/jhj/algorithm/leetcode/Leetcode.java
@@ -2,42 +2,25 @@ package com.jhj.algorithm.leetcode;
 
 
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
 
 public class Leetcode {
     class Solution {
-        ArrayList<String> res = new ArrayList<String>();
-
-        public List<String> letterCombinations(String digits) {
-            if (digits.length() == 0) {
-                return res;
-            }
-            HashMap<Character, String> map = new HashMap<>();
-            map.put('1', "");
-            map.put('2', "abc");
-            map.put('3', "def");
-            map.put('4', "ghi");
-            map.put('5', "jkl");
-            map.put('6', "mno");
-            map.put('7', "pqrs");
-            map.put('8', "tuv");
-            map.put('9', "wxyz");
-            huisu(new StringBuilder(), digits, 0, map);
+        ArrayList<List<Integer>> res=new ArrayList<List<Integer>>();
+        public List<List<Integer>> combine(int n, int k) {
+            huisu(k,new ArrayList<>(),1,n);
             return res;
         }
 
-        public void huisu(StringBuilder path, String digits, int start, HashMap<Character, String> map) {
-
-            if (path.length() == digits.length()) {
-                res.add(new String(path.toString()));
+        public void huisu(int k,ArrayList<Integer> path,int start,int n){
+            if(path.size()==k){
+                res.add(new ArrayList<>(path));
                 return;
             }
-            String s = map.get(digits.charAt(start));
-            for (int j = 0; j < s.length(); j++) {
-                path.append(s.charAt(j));
-                huisu(path, digits, start + 1, map);
-                path.deleteCharAt(path.length() - 1);
+            for(int i=start;i<=n;i++){
+                path.add(i);
+                huisu(k,path,i+1,n);
+                path.remove(path.size()-1);
             }
         }
     }
