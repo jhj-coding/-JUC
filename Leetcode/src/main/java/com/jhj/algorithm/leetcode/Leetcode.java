5223a140524301c8d56017fb530a85406ea19553
jhj <hnjzjhj@163.com>
2024-03-24 18:01:51 +0800

    leetcode 322

--- Leetcode/src/main/java/com/jhj/algorithm/leetcode/Leetcode.java
+++ Leetcode/src/main/java/com/jhj/algorithm/leetcode/Leetcode.java
@@ -1,36 +1,23 @@
 package com.jhj.algorithm.leetcode;
 
 
-import java.util.ArrayList;
-import java.util.Comparator;
+import java.util.Arrays;
 
 class Solution {
-    public class ListNode {
-  int val;
-  ListNode next;
-  ListNode() {}
-  ListNode(int val) { this.val = val; }
-  ListNode(int val, ListNode next) { this.val = val; this.next = next; }
-  }
-    public ListNode sortList(ListNode head) {
-        ArrayList<Integer> res = new ArrayList<>();
-        ListNode cur = head;
-        while (cur != null) {
-            res.add(cur.val);
-            cur = cur.next;
-        }
-        res.sort(new Comparator<Integer>() {
-            @Override
-            public int compare(Integer o1, Integer o2) {
-                return o1 - o2;
+    public int coinChange(int[] coins, int amount) {
+        int[] f = new int[amount + 1];
+        Arrays.fill(f, Integer.MAX_VALUE);
+        f[0] = 0;
+        for (int i = 1; i <= amount; i++) {
+            for (int j = 0; j < coins.length; j++) {
+                if (coins[j] <= i && f[i - coins[j]] != Integer.MAX_VALUE) {
+                    f[i] = Math.min(f[i - coins[j]] + 1, f[i]);
+                }
             }
-        });
-        ListNode pre = new ListNode(-1);
-        ListNode curr = pre;
-        for (int i = 0; i < res.size(); i++) {
-            curr.next = new ListNode(res.get(i));
-            curr = curr.next;
         }
-        return pre.next;
+        if (f[amount] == Integer.MAX_VALUE) {
+            return -1;
+        }
+        return f[amount];
     }
 }
