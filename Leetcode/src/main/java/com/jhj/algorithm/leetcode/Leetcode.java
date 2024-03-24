9c5909e2e3a835c74c1bcec7de839458633b679c
jhj <hnjzjhj@163.com>
2024-03-24 16:42:52 +0800

    leetcode 22

--- Leetcode/src/main/java/com/jhj/algorithm/leetcode/Leetcode.java
+++ Leetcode/src/main/java/com/jhj/algorithm/leetcode/Leetcode.java
@@ -1,51 +1,34 @@
 package com.jhj.algorithm.leetcode;
 
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Leetcode {
     class Solution {
-        int res=0;
-        public int totalNQueens(int n) {
-            char[][] chars = new char[n][n];
-            for(int i=0;i<n;i++){
-                for(int j=0;j<n;j++){
-                    chars[i][j]='.';
-                }
-            }
-            huisu(n,0,chars);
+        List<String> res=new ArrayList<>();
+        public List<String> generateParenthesis(int n) {
+
+            dfs(n,n,new StringBuilder());
             return res;
         }
-        public void huisu(int n,int rowIndex,char[][] chars){
-            if(n==rowIndex){
-                res++;
+
+        private void dfs(int left, int right, StringBuilder stringBuilder) {
+            if(left==right&&left==0){
+                res.add(stringBuilder.toString());
                 return;
             }
-            for(int i=0;i<n;i++){
-                boolean flag=true;
-                for(int j=0;j<rowIndex;j++){
-                    if(chars[j][i]=='Q'){
-                        flag=false;
-                        break;
-                    }
-                }
-                for (int h = rowIndex - 1, l = i - 1; l >= 0 && h >= 0; l--, h--) {
-                    if (chars[h][l] == 'Q') {
-                        flag = false;
-                        break;
-                    }
-                }
-
-                for (int h = rowIndex - 1, l = i + 1; h >= 0 && l < n; l++, h--) {
-                    if (chars[h][l] == 'Q') {
-                        flag = false;
-                        break;
-                    }
-                }
-                if (flag) {
-                    chars[rowIndex][i] = 'Q';
-                    huisu(n, rowIndex + 1, chars);
-                    chars[rowIndex][i] = '.';
-                }
+            if(left>0){
+                stringBuilder.append("(");
+                dfs(left-1,right,stringBuilder);
+                stringBuilder.deleteCharAt(stringBuilder.length()-1);
+            }
+            if(right>0 && left<right){
+                stringBuilder.append(")");
+                dfs(left,right-1,stringBuilder);
+                stringBuilder.deleteCharAt(stringBuilder.length()-1);
             }
         }
+
     }
 }
