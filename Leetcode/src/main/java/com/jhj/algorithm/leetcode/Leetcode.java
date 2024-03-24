e4d09e11cae1d54ec4d0be38dd1cdc6a9aa80b97
jhj <hnjzjhj@163.com>
2024-03-24 17:06:27 +0800

    leetcode 108

--- Leetcode/src/main/java/com/jhj/algorithm/leetcode/Leetcode.java
+++ Leetcode/src/main/java/com/jhj/algorithm/leetcode/Leetcode.java
@@ -3,43 +3,31 @@ package com.jhj.algorithm.leetcode;
 
 public class Leetcode {
     class Solution {
-        public boolean exist(char[][] board, String word) {
-            int h = board.length, w = board[0].length;
-            boolean[][] visited = new boolean[h][w];
-            for (int i = 0; i < h; i++) {
-                for (int j = 0; j < w; j++) {
-                    boolean flag = check(board, visited, i, j, word, 0);
-                    if (flag) {
-                        return true;
-                    }
-                }
-            }
-            return false;
+        public class TreeNode {
+      int val;
+      TreeNode left;
+      TreeNode right;
+      TreeNode() {}
+      TreeNode(int val) { this.val = val; }
+      TreeNode(int val, TreeNode left, TreeNode right) {
+          this.val = val;
+          this.left = left;
+          this.right = right;
+      }
+  }
+        public TreeNode sortedArrayToBST(int[] nums) {
+            return sortedArrayToBST(nums,0,nums.length-1);
         }
 
-        public boolean check(char[][] board, boolean[][] visited, int i, int j, String s, int k) {
-            if (board[i][j] != s.charAt(k)) {
-                return false;
-            } else if (k == s.length() - 1) {
-                return true;
+        public TreeNode sortedArrayToBST(int[] nums,int left,int right) {
+            if(left>right){
+                return null;
             }
-            visited[i][j] = true;
-            int[][] directions = {{0, 1}, {0, -1}, {1, 0}, {-1, 0}};
-            boolean result = false;
-            for (int[] dir : directions) {
-                int newi = i + dir[0], newj = j + dir[1];
-                if (newi >= 0 && newi < board.length && newj >= 0 && newj < board[0].length) {
-                    if (!visited[newi][newj]) {
-                        boolean flag = check(board, visited, newi, newj, s, k + 1);
-                        if (flag) {
-                            result = true;
-                            break;
-                        }
-                    }
-                }
-            }
-            visited[i][j] = false;
-            return result;
+            int mid=(left+right)>>1;
+            TreeNode root = new TreeNode(nums[mid]);
+            root.left=sortedArrayToBST(nums,left,mid-1);
+            root.right=sortedArrayToBST(nums,mid+1,right);
+            return root;
         }
     }
 }
