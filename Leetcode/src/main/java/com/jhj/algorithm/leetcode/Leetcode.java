37f15bb0a46d033fcf309a4c4619e2729d39e0d8
jhj <hnjzjhj@163.com>
2024-03-25 11:59:24 +0800

    leetcode 53

--- Leetcode/src/main/java/com/jhj/algorithm/leetcode/Leetcode.java
+++ Leetcode/src/main/java/com/jhj/algorithm/leetcode/Leetcode.java
@@ -2,56 +2,16 @@ package com.jhj.algorithm.leetcode;
 
 
 class Solution {
-    public class ListNode {
-      int val;
-      ListNode next;
-      ListNode() {}
-      ListNode(int val) { this.val = val; }
-      ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+  public int maxSubArray(int[] nums) {
+    int res=nums[0];
+    int sum=0;
+    for(int i=0;i<nums.length;i++){
+      sum+=nums[i];
+      if(sum<nums[i]){
+        sum=nums[i];
+      }
+      res=Math.max(res,sum);
+    }
+    return res;
   }
-    public ListNode mergeKLists(ListNode[] lists) {
-      if(lists.length==0){
-        return null;
-      }
-      return merge(lists,0,lists.length-1);
-    }
-
-    public ListNode merge(ListNode[] lists,int left,int right){
-      if(left>right){
-        return null;
-      }
-      if(left==right){
-        return lists[left];
-      }
-      int mid=(left+right)>>1;
-      return mergeTwo(merge(lists,left,mid),merge(lists,mid+1,right));
-    }
-    public ListNode mergeTwo(ListNode l1,ListNode l2){
-      ListNode ppre=new ListNode(-1);
-      ListNode pre=ppre;
-      ListNode cur1=l1;
-      ListNode cur2=l2;
-      while (cur1!=null||cur2!=null){
-        if(cur1!=null&&cur2!=null){
-          if(cur1.val<cur2.val){
-            pre.next=new ListNode(cur1.val);
-            cur1=cur1.next;
-            pre=pre.next;
-          }else{
-            pre.next=new ListNode(cur2.val);
-            cur2=cur2.next;
-            pre=pre.next;
-          }
-        }else if(cur1!=null){
-          pre.next=new ListNode(cur1.val);
-          cur1=cur1.next;
-          pre=pre.next;
-        }else{
-          pre.next=new ListNode(cur2.val);
-          cur2=cur2.next;
-          pre=pre.next;
-        }
-      }
-      return ppre.next;
-    }
 }
