e32e3a5dc9be055eb327f013828deee58c6c29d6
jhj <hnjzjhj@163.com>
2024-03-25 12:38:21 +0800

    leetcode 918

--- Leetcode/src/main/java/com/jhj/algorithm/leetcode/Leetcode.java
+++ Leetcode/src/main/java/com/jhj/algorithm/leetcode/Leetcode.java
@@ -2,16 +2,31 @@ package com.jhj.algorithm.leetcode;
 
 
 class Solution {
-  public int maxSubArray(int[] nums) {
-    int res=nums[0];
-    int sum=0;
-    for(int i=0;i<nums.length;i++){
-      sum+=nums[i];
-      if(sum<nums[i]){
-        sum=nums[i];
+  public int maxSubarraySumCircular(int[] nums) {
+    int max = nums[0];
+    int sum = 0;
+    for (int i = 0; i < nums.length; i++) {
+      sum += nums[i];
+      if (sum < nums[i]) {
+        sum = nums[i];
       }
-      res=Math.max(res,sum);
+      max = Math.max(max, sum);
     }
-    return res;
+    int min = nums[0];
+    int summin = 0;
+    int s = 0;
+    for (int i = 0; i < nums.length; i++) {
+      s += nums[i];
+      summin+=nums[i];
+      if (summin > nums[i]) {
+        summin = nums[i];
+      }
+      min = Math.min(min, summin);
+    }
+    if (max < 0) {
+      return max;
+    } else {
+      return Math.max(max, s - min);
+    }
   }
 }
