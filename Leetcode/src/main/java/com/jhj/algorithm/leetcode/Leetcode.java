532f7617110551e42f07eb29e16b3ac9bf026138
jhj <hnjzjhj@163.com>
2024-03-25 13:45:21 +0800

    leetcode 427

--- Leetcode/src/main/java/com/jhj/algorithm/leetcode/Leetcode.java
+++ Leetcode/src/main/java/com/jhj/algorithm/leetcode/Leetcode.java
@@ -2,19 +2,72 @@ package com.jhj.algorithm.leetcode;
 
 
 class Solution {
-  public int searchInsert(int[] nums, int target) {
-    int left=0;
-    int right=nums.length-1;
-    while (left<=right){
-      int mid=(left+right)>>1;
-      if(nums[mid]==target){
-        return mid;
-      }else if(nums[mid]>target){
-        right=mid-1;
-      }else{
-        left=mid+1;
-      }
+  class Node {
+    public boolean val;
+    public boolean isLeaf;
+    public Node topLeft;
+    public Node topRight;
+    public Node bottomLeft;
+    public Node bottomRight;
+
+
+    public Node() {
+      this.val = false;
+      this.isLeaf = false;
+      this.topLeft = null;
+      this.topRight = null;
+      this.bottomLeft = null;
+      this.bottomRight = null;
     }
-    return left;
+
+    public Node(boolean val, boolean isLeaf) {
+      this.val = val;
+      this.isLeaf = isLeaf;
+      this.topLeft = null;
+      this.topRight = null;
+      this.bottomLeft = null;
+      this.bottomRight = null;
+    }
+
+    public Node(boolean val, boolean isLeaf, Node topLeft, Node topRight, Node bottomLeft, Node bottomRight) {
+      this.val = val;
+      this.isLeaf = isLeaf;
+      this.topLeft = topLeft;
+      this.topRight = topRight;
+      this.bottomLeft = bottomLeft;
+      this.bottomRight = bottomRight;
+    }
   }
+    public Node construct(int[][] grid) {
+      return dfs(grid, 0, 0, grid.length, grid.length);
+    }
+
+    public Node dfs(int[][] grid, int r0, int c0, int r1, int c1) {
+      boolean same = true;
+      for (int i = r0; i < r1; ++i) {
+        for (int j = c0; j < c1; ++j) {
+          if (grid[i][j] != grid[r0][c0]) {
+            same = false;
+            break;
+          }
+        }
+        if (!same) {
+          break;
+        }
+      }
+
+      if (same) {
+        return new Node(grid[r0][c0] == 1, true);
+      }
+
+      Node ret = new Node(
+              true,
+              false,
+              dfs(grid, r0, c0, (r0 + r1) / 2, (c0 + c1) / 2),
+              dfs(grid, r0, (c0 + c1) / 2, (r0 + r1) / 2, c1),
+              dfs(grid, (r0 + r1) / 2, c0, r1, (c0 + c1) / 2),
+              dfs(grid, (r0 + r1) / 2, (c0 + c1) / 2, r1, c1)
+      );
+      return ret;
+    }
 }
