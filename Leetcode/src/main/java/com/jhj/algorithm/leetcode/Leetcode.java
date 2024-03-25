ff39678fa514c55825b1e08eda25ad9a8d2636ce
jhj <hnjzjhj@163.com>
2024-03-25 11:57:06 +0800

    leetcode 23

--- Leetcode/src/main/java/com/jhj/algorithm/leetcode/Leetcode.java
+++ Leetcode/src/main/java/com/jhj/algorithm/leetcode/Leetcode.java
@@ -1,19 +1,57 @@
 package com.jhj.algorithm.leetcode;
 
 
-import java.util.Arrays;
-
 class Solution {
-        public int change(int amount, int[] coins) {
-            int[] f = new int[amount + 1];
-            Arrays.fill(f,0);
-            f[0]=1;
-            for(int i=0;i<coins.length;i++){
-                for(int j=coins[i];j<=amount;j++){
-                    f[j]=f[j]+f[j-coins[i]];
-                }
-            }
-            return f[amount];
-        }
+    public class ListNode {
+      int val;
+      ListNode next;
+      ListNode() {}
+      ListNode(int val) { this.val = val; }
+      ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+  }
+    public ListNode mergeKLists(ListNode[] lists) {
+      if(lists.length==0){
+        return null;
+      }
+      return merge(lists,0,lists.length-1);
+    }
 
+    public ListNode merge(ListNode[] lists,int left,int right){
+      if(left>right){
+        return null;
+      }
+      if(left==right){
+        return lists[left];
+      }
+      int mid=(left+right)>>1;
+      return mergeTwo(merge(lists,left,mid),merge(lists,mid+1,right));
+    }
+    public ListNode mergeTwo(ListNode l1,ListNode l2){
+      ListNode ppre=new ListNode(-1);
+      ListNode pre=ppre;
+      ListNode cur1=l1;
+      ListNode cur2=l2;
+      while (cur1!=null||cur2!=null){
+        if(cur1!=null&&cur2!=null){
+          if(cur1.val<cur2.val){
+            pre.next=new ListNode(cur1.val);
+            cur1=cur1.next;
+            pre=pre.next;
+          }else{
+            pre.next=new ListNode(cur2.val);
+            cur2=cur2.next;
+            pre=pre.next;
+          }
+        }else if(cur1!=null){
+          pre.next=new ListNode(cur1.val);
+          cur1=cur1.next;
+          pre=pre.next;
+        }else{
+          pre.next=new ListNode(cur2.val);
+          cur2=cur2.next;
+          pre=pre.next;
+        }
+      }
+      return ppre.next;
+    }
 }
