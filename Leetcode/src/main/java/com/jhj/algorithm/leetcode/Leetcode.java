b84a68a8695e00f5faee43437ff48bb654091657
jhj <hnjzjhj@163.com>
2024-03-25 11:37:13 +0800

    leetcode 518

--- Leetcode/src/main/java/com/jhj/algorithm/leetcode/Leetcode.java
+++ Leetcode/src/main/java/com/jhj/algorithm/leetcode/Leetcode.java
@@ -4,20 +4,16 @@ package com.jhj.algorithm.leetcode;
 import java.util.Arrays;
 
 class Solution {
-    public int coinChange(int[] coins, int amount) {
-        int[] f = new int[amount + 1];
-        Arrays.fill(f, Integer.MAX_VALUE);
-        f[0] = 0;
-        for (int i = 1; i <= amount; i++) {
-            for (int j = 0; j < coins.length; j++) {
-                if (coins[j] <= i && f[i - coins[j]] != Integer.MAX_VALUE) {
-                    f[i] = Math.min(f[i - coins[j]] + 1, f[i]);
+        public int change(int amount, int[] coins) {
+            int[] f = new int[amount + 1];
+            Arrays.fill(f,0);
+            f[0]=1;
+            for(int i=0;i<coins.length;i++){
+                for(int j=coins[i];j<=amount;j++){
+                    f[j]=f[j]+f[j-coins[i]];
                 }
             }
+            return f[amount];
         }
-        if (f[amount] == Integer.MAX_VALUE) {
-            return -1;
-        }
-        return f[amount];
-    }
+
 }
