ec9ce3fe31c6d445e300bd3ce958f592aafa6a3b
jhj <hnjzjhj@163.com>
2024-03-27 17:41:38 +0800

    leetcode 74

--- Leetcode/src/main/java/com/jhj/algorithm/leetcode/Leetcode.java
+++ Leetcode/src/main/java/com/jhj/algorithm/leetcode/Leetcode.java
@@ -1,52 +1,20 @@
 package com.jhj.algorithm.leetcode;
 
-import java.util.ArrayList;
-import java.util.Comparator;
-
 class Solution {
-  public int countWays(int[][] ranges) {
-    ArrayList<int[]> ints = new ArrayList<>();
-    for (int i = 0; i < ranges.length; i++) {
-      ints.add(ranges[i]);
-    }
-    ints.sort(new Comparator<int[]>() {
-      @Override
-      public int compare(int[] o1, int[] o2) {
-        if (o1[0] == o2[0]) {
-          return o1[1] - o2[1];
-        }
-        return o1[0] - o2[0];
-      }
-    });
-    ArrayList<int[]> res = new ArrayList<>();
-    res.add(ints.get(0));
-    for (int i = 1; i < ints.size(); i++) {
-      if (ints.get(i)[0] <= res.get(res.size() - 1)[1]) {
-        res.get(res.size() - 1)[1] = Math.max(res.get(res.size() - 1)[1], ints.get(i)[1]);
-      } else {
-        res.add(ints.get(i));
+  public boolean searchMatrix(int[][] matrix, int target) {
+    int n=matrix.length;
+    int m=matrix[0].length;
+    int starthang=0;
+    int startlie=m-1;
+    while (startlie>=0&&starthang<n){
+      if(matrix[starthang][startlie]==target){
+        return true;
+      }else if(matrix[starthang][startlie]>target){
+        startlie--;
+      }else{
+        starthang++;
       }
     }
-    System.out.print(quickPow(res.size()));
-    return (int)quickPow(res.size());
-  }
-
-  public long quickPow(int n) {
-    long mod = 1000000007;
-    long res = 1;
-    long x = 2;
-    while (n != 0) {
-      if (n % 2 == 1) {
-        res *= x;
-        res %= mod;
-        x *= x;
-        x %= mod;
-      } else {
-        x *= x;
-        x %= mod;
-      }
-      n /= 2;
-    }
-    return res;
+    return false;
   }
 }
