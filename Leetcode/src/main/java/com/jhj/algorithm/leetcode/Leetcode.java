d3bd89d6a3efd5d800b1902dce0ede29f8f20551
jhj <hnjzjhj@163.com>
2024-03-27 19:50:18 +0800

    leetcode 34

--- Leetcode/src/main/java/com/jhj/algorithm/leetcode/Leetcode.java
+++ Leetcode/src/main/java/com/jhj/algorithm/leetcode/Leetcode.java
@@ -1,28 +1,35 @@
 package com.jhj.algorithm.leetcode;
 
 class Solution {
-  public int search(int[] nums, int target) {
-    int left=0;
-    int right=nums.length-1;
-    while (left<=right){
-      int mid=(left+right)>>1;
-      if(nums[mid]==target){
-        return mid;
-      }
-      if(nums[mid]>=nums[0]){
-        if(target>=nums[0]&&target<nums[mid]){
-          right=mid-1;
-        }else{
-          left=mid+1;
-        }
-      }else{
-        if(target<=nums[nums.length-1]&&target>nums[mid]){
-          left=mid+1;
-        }else {
-          right=mid-1;
-        }
+  public int[] searchRange(int[] nums, int target) {
+    if(nums.length==0){
+      return new int[]{-1,-1};
+    }
+    int left = 0;
+    int right = nums.length - 1;
+    while (left <= right) {
+      int mid = (left + right) >> 1;
+      if (target > nums[mid]) {
+        left = mid + 1;
+      } else {
+        right = mid - 1;
       }
     }
-    return -1;
+    int temp = right+1;
+    left = 0;
+    right = nums.length - 1;
+    while (left <= right) {
+      int mid = (left + right) >> 1;
+      if (target >= nums[mid]) {
+        left = mid + 1;
+      } else {
+        right = mid - 1;
+      }
+    }
+    if(temp<nums.length&&left>=1&&temp<left&&nums[temp]==target){
+      return new int[]{temp,left-1};
+    }else{
+      return new int[]{-1,-1};
+    }
   }
 }
