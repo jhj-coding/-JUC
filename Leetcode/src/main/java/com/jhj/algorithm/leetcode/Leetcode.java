88ff8392f56fb4b45415e78ce1932df56a8e427e
jhj <hnjzjhj@163.com>
2024-03-27 19:21:58 +0800

    leetcode 162

--- Leetcode/src/main/java/com/jhj/algorithm/leetcode/Leetcode.java
+++ Leetcode/src/main/java/com/jhj/algorithm/leetcode/Leetcode.java
@@ -2,24 +2,16 @@ package com.jhj.algorithm.leetcode;
 
 class Solution {
   public int findPeakElement(int[] nums) {
-    if(nums.length==1){
-      return 0;
-    }
-    for (int i = 0; i < nums.length; i++) {
-      if (i == 0) {
-        if (nums[i] > nums[i + 1]) {
-          return i;
-        }
-      } else if (i == nums.length - 1) {
-        if (nums[i] > nums[i - 1]) {
-          return i;
-        }
-      } else {
-        if (nums[i] > nums[i - 1] && nums[i] > nums[i + 1]) {
-          return i;
-        }
+    int left=0;
+    int right=nums.length-1;
+    while (left<right){
+      int mid=(left+right)>>1;
+      if(nums[mid]>nums[mid+1]){
+        right=mid;
+      }else{
+        left=mid+1;
       }
     }
-    return -1;
+    return right;
   }
 }
