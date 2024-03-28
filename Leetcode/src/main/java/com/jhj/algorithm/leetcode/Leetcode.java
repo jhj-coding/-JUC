fe999f2e2e4a1ed1891d1bf7042724f39364c43e
jhj <hnjzjhj@163.com>
2024-03-28 20:49:10 +0800

    leetcode 1997

--- Leetcode/src/main/java/com/jhj/algorithm/leetcode/Leetcode.java
+++ Leetcode/src/main/java/com/jhj/algorithm/leetcode/Leetcode.java
@@ -1,17 +1,14 @@
 package com.jhj.algorithm.leetcode;
 
 class Solution {
-  public int findMin(int[] nums) {
-    int left=0;
-    int right=nums.length-1;
-    while (left<right){
-      int mid=(left+right)>>1;
-      if(nums[mid]<nums[right]){
-        right=mid;
-      }else{
-        left=mid+1;
-      }
+  public int firstDayBeenInAllRooms(int[] nextVisit) {
+    final long MOD = 1_000_000_007;
+    int n = nextVisit.length;
+    long[] s = new long[n];
+    for (int i = 0; i < n - 1; i++) {
+      int j = nextVisit[i];
+      s[i + 1] = (s[i] * 2 - s[j] + 2 + MOD) % MOD; // + MOD 避免算出负数
     }
-    return nums[left];
+    return (int) s[n - 1];
   }
 }
