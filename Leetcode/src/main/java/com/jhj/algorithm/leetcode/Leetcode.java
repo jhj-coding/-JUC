a7cb06f34b1ac48a78d53592857c5d831b6fae91
jhj <hnjzjhj@163.com>
2024-03-30 14:18:55 +0800

    leetcode 502

--- Leetcode/src/main/java/com/jhj/algorithm/leetcode/Leetcode.java
+++ Leetcode/src/main/java/com/jhj/algorithm/leetcode/Leetcode.java
@@ -1,31 +1,33 @@
 package com.jhj.algorithm.leetcode;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.PriorityQueue;
+import java.util.*;
 
 class Solution {
-  public List<List<Integer>> kSmallestPairs(int[] nums1, int[] nums2, int k) {
-    PriorityQueue<int[]> pq = new PriorityQueue<>(k, (o1, o2)->{
-      return nums1[o1[0]] + nums2[o1[1]] - nums1[o2[0]] - nums2[o2[1]];
-    });
-    List<List<Integer>> ans = new ArrayList<>();
-    int m = nums1.length;
-    int n = nums2.length;
-    for (int i = 0; i < Math.min(m, k); i++) {
-      pq.offer(new int[]{i,0});
+  public int findMaximizedCapital(int k, int w, int[] profits, int[] capital) {
+    int n=profits.length;
+    int[][] arr = new int[n][2];
+    for(int i=0;i<n;i++){
+      arr[i]=new int[]{capital[i],profits[i]};
     }
-    while (k-- > 0 && !pq.isEmpty()) {
-      int[] idxPair = pq.poll();
-      List<Integer> list = new ArrayList<>();
-      list.add(nums1[idxPair[0]]);
-      list.add(nums2[idxPair[1]]);
-      ans.add(list);
-      if (idxPair[1] + 1 < n) {
-        pq.offer(new int[]{idxPair[0], idxPair[1] + 1});
+    Arrays.sort(arr, new Comparator<int[]>() {
+      @Override
+      public int compare(int[] o1, int[] o2) {
+        return o1[0]-o2[0];
+      }
+    });
+    PriorityQueue<Integer> priorityQueue = new PriorityQueue<>((x, y) -> y - x);
+    int curr=0;
+    for(int i=0;i<k;i++){
+      while (curr<n&&arr[curr][0]<=w){
+        priorityQueue.add(arr[curr][1]);
+        curr++;
+      }
+      if (!priorityQueue.isEmpty()) {
+        w += priorityQueue.poll();
+      } else {
+        break;
       }
     }
-
-    return ans;
+    return w;
   }
 }
