0a788b13f7116871ba0ea71eeccc897a75521d7a
jhj <hnjzjhj@163.com>
2024-03-30 15:06:33 +0800

    leetcode 507

--- Leetcode/src/main/java/com/jhj/algorithm/leetcode/Leetcode.java
+++ Leetcode/src/main/java/com/jhj/algorithm/leetcode/Leetcode.java
@@ -1,39 +1,13 @@
 package com.jhj.algorithm.leetcode;
 
-import java.util.*;
-
-class MedianFinder {
-  PriorityQueue<Integer> leftQueue;
-  PriorityQueue<Integer> rightQueue;
-  public MedianFinder() {
-    leftQueue=new PriorityQueue<>(new Comparator<Integer>() {
-      @Override
-      public int compare(Integer o1, Integer o2) {
-        return o2-o1;
-      }
-    });
-    rightQueue=new PriorityQueue<>();
-  }
-
-  public void addNum(int num) {
-    if(leftQueue.isEmpty() || num<=leftQueue.peek()){
-      leftQueue.add(num);
-      while (leftQueue.size()-rightQueue.size()>1){
-        rightQueue.add(leftQueue.remove());
-      }
-    }else {
-      rightQueue.add(num);
-      while (rightQueue.size() - leftQueue.size() > 0) {
-        leftQueue.add(rightQueue.remove());
+class Solution {
+  public boolean checkPerfectNumber(int num) {
+    int s=0;
+    for(int i=1;i<num;i++){
+      if(num%i==0){
+        s+=i;
       }
     }
-  }
-
-  public double findMedian() {
-    if(leftQueue.size()>rightQueue.size()){
-      return leftQueue.peek();
-    }else{
-      return (leftQueue.peek()+rightQueue.peek())/2.0;
-    }
+    return s==num;
   }
 }
