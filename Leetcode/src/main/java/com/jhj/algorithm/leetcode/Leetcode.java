2905ffdc32ef91f1376b462a9c4dc5ae36ccf8e1
jhj <hnjzjhj@163.com>
2024-03-31 16:33:54 +0800

    leetcode 331

--- Leetcode/src/main/java/com/jhj/algorithm/leetcode/Leetcode.java
+++ Leetcode/src/main/java/com/jhj/algorithm/leetcode/Leetcode.java
@@ -1,20 +1,26 @@
 package com.jhj.algorithm.leetcode;
-
 class Solution {
-  public int fib(int n) {
-    if(n==0){
-      return 0;
+  public boolean isValidSerialization(String preorder) {
+    int n = preorder.length();
+    int i = 0;
+    int slots = 1;
+    while (i < n) {
+      if (slots == 0) {
+        return false;
+      }
+      if (preorder.charAt(i) == ',') {
+        i++;
+      } else if (preorder.charAt(i) == '#'){
+        slots--;
+        i++;
+      } else {
+        // 读一个数字
+        while (i < n && preorder.charAt(i) != ',') {
+          i++;
+        }
+        slots++; // slots = slots - 1 + 2
+      }
     }
-    if(n<=1){
-      return 1;
-    }
-    int a=0;
-    int b=1;
-    for(int i=0;i<n;i++){
-      int temp=a;
-      a=a+b;
-      b=temp;
-    }
-    return a;
+    return slots == 0;
   }
 }
