5ceabae08d372a752f108d9cdd486930997e2c99
jhj <hnjzjhj@163.com>
2024-04-08 16:03:58 +0800

    leetcode 191

--- Leetcode/src/main/java/com/jhj/algorithm/leetcode/Leetcode.java
+++ Leetcode/src/main/java/com/jhj/algorithm/leetcode/Leetcode.java
@@ -1,12 +1,13 @@
 package com.jhj.algorithm.leetcode;
 
 class Solution {
-  public int reverseBits(int n) {
-    int rev = 0;
-    for (int i = 0; i < 32 && n != 0; ++i) {
-      rev |= (n & 1) << (31 - i);
-      n >>>= 1;
+  public int hammingWeight(int n) {
+    int count=0;
+    for(int i=0;i<=31;i++){
+      if((n&(1<<i))!=0){
+        count++;
+      }
     }
-    return rev;
+    return count;
   }
 }
