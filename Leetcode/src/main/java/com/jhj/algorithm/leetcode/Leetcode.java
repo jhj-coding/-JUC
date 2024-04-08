5c1281e4db9dbb7db9ceef3c5b150cf64a7bf090
jhj <hnjzjhj@163.com>
2024-04-08 15:57:44 +0800

    leetcode 190

--- Leetcode/src/main/java/com/jhj/algorithm/leetcode/Leetcode.java
+++ Leetcode/src/main/java/com/jhj/algorithm/leetcode/Leetcode.java
@@ -1,33 +1,12 @@
 package com.jhj.algorithm.leetcode;
 
 class Solution {
-  public String addBinary(String a, String b) {
-    int lena=a.length();
-    int lenb=b.length();
-    StringBuilder s = new StringBuilder();
-    int res=0;
-    while (lena>0||lenb>0){
-      if(lena>0&&lenb>0){
-        char ca = a.charAt(--lena);
-        char cb = b.charAt(--lenb);
-        res+=(ca-'0')+(cb-'0');
-        s.insert(0,res%2);
-        res/=2;
-      }else if(lena>0){
-        char ca = a.charAt(--lena);
-        res+=(ca-'0');
-        s.insert(0,res%2);
-        res/=2;
-      }else{
-        char cb = b.charAt(--lenb);
-        res+=(cb-'0');
-        s.insert(0,res%2);
-        res/=2;
-      }
+  public int reverseBits(int n) {
+    int rev = 0;
+    for (int i = 0; i < 32 && n != 0; ++i) {
+      rev |= (n & 1) << (31 - i);
+      n >>>= 1;
     }
-    if(res!=0){
-      s.insert(0,res%2);
-    }
-    return s.toString();
+    return rev;
   }
 }
