5c21751983840313bb50a4f4c221f6435ae91eb9
jhj <hnjzjhj@163.com>
2024-04-09 11:24:01 +0800

    leetcode 66

--- Leetcode/src/main/java/com/jhj/algorithm/leetcode/Leetcode.java
+++ Leetcode/src/main/java/com/jhj/algorithm/leetcode/Leetcode.java
@@ -1,16 +1,22 @@
 package com.jhj.algorithm.leetcode;
 
 class Solution {
-    public boolean isPalindrome(int x) {
-        if (x < 0) {
-            return false;
+    public int[] plusOne(int[] digits) {
+        int res=1;
+        int len=digits.length;
+        for(int i=len-1;i>=0;i--){
+            res=digits[i]+res;
+            digits[i]=res%10;
+            res/=10;
         }
-        int c = x;
-        int res = 0;
-        while (c != 0) {
-            res = res * 10 + c % 10;
-            c /= 10;
+        if(res==0){
+            return digits;
         }
-        return res == x;
+        int[] ints = new int[len + 1];
+        ints[0]=res;
+        for (int i=1;i<len+1;i++){
+            ints[i]=digits[i-1];
+        }
+        return ints;
     }
 }
