e768febe51b88aaba4c550cdea89e0c1fb7beedb
jhj <hnjzjhj@163.com>
2024-04-09 11:30:01 +0800

    leetcode 172

--- Leetcode/src/main/java/com/jhj/algorithm/leetcode/Leetcode.java
+++ Leetcode/src/main/java/com/jhj/algorithm/leetcode/Leetcode.java
@@ -1,22 +1,12 @@
 package com.jhj.algorithm.leetcode;
 
 class Solution {
-    public int[] plusOne(int[] digits) {
-        int res=1;
-        int len=digits.length;
-        for(int i=len-1;i>=0;i--){
-            res=digits[i]+res;
-            digits[i]=res%10;
-            res/=10;
+    public int trailingZeroes(int n) {
+        int ans = 0;
+        while (n != 0) {
+            n /= 5;
+            ans += n;
         }
-        if(res==0){
-            return digits;
-        }
-        int[] ints = new int[len + 1];
-        ints[0]=res;
-        for (int i=1;i<len+1;i++){
-            ints[i]=digits[i-1];
-        }
-        return ints;
+        return ans;
     }
 }
