581120325df57f127ba016433d9d32b9ee959e7f
jhj <hnjzjhj@163.com>
2024-04-10 13:55:01 +0800

    leetcode 1702

--- Leetcode/src/main/java/com/jhj/algorithm/leetcode/Leetcode.java
+++ Leetcode/src/main/java/com/jhj/algorithm/leetcode/Leetcode.java
@@ -1,31 +1,22 @@
 package com.jhj.algorithm.leetcode;
 
 class Solution {
-    public int uniquePathsWithObstacles(int[][] obstacleGrid) {
-        if(obstacleGrid[0][0]==1){
-            return 0;
-        }
-        int m=obstacleGrid.length;
-        int n=obstacleGrid[0].length;
-        int[][] f = new int[m][n];
-        f[0][0]=1;
-        for(int i=1;i<n;i++){
-            if(obstacleGrid[0][i]==0){
-                f[0][i]=f[0][i-1];
-            }
-        }
-        for(int i=1;i<m;i++){
-            if(obstacleGrid[i][0]==0){
-                f[i][0]=f[i-1][0];
-            }
-        }
-        for(int i=1;i<m;i++){
-            for(int j=1;j<n;j++){
-                if(obstacleGrid[i][j]==0) {
-                    f[i][j] = f[i][j - 1] + f[i - 1][j];
+    public String maximumBinaryString(String binary) {
+        char[] s = binary.toCharArray();
+        int n=binary.length();
+        int j=0;
+        for(int i=0;i<n;i++){
+            if(s[i]=='0'){
+                while (j<=i||(j<n&&s[j]=='1')){
+                    j++;
+                }
+                if(j<n){
+                    s[j]='1';
+                    s[i]='1';
+                    s[i+1]='0';
                 }
             }
         }
-        return f[m-1][n-1];
+        return new String(s);
     }
 }
