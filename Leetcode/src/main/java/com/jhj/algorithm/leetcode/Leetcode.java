6f483841f524e16c20d7eab769f9726629b1aaa7
jhj <hnjzjhj@163.com>
2024-04-10 14:43:09 +0800

    leetcode 438

--- Leetcode/src/main/java/com/jhj/algorithm/leetcode/Leetcode.java
+++ Leetcode/src/main/java/com/jhj/algorithm/leetcode/Leetcode.java
@@ -1,22 +1,54 @@
 package com.jhj.algorithm.leetcode;
 
+import java.util.ArrayList;
+import java.util.List;
+
 class Solution {
-    public String maximumBinaryString(String binary) {
-        char[] s = binary.toCharArray();
-        int n=binary.length();
-        int j=0;
-        for(int i=0;i<n;i++){
-            if(s[i]=='0'){
-                while (j<=i||(j<n&&s[j]=='1')){
-                    j++;
-                }
-                if(j<n){
-                    s[j]='1';
-                    s[i]='1';
-                    s[i+1]='0';
-                }
+    public List<Integer> findAnagrams(String s, String p) {
+        int sLen = s.length(), pLen = p.length();
+
+        if (sLen < pLen) {
+            return new ArrayList<Integer>();
+        }
+
+        List<Integer> ans = new ArrayList<Integer>();
+        int[] count = new int[26];
+        for (int i = 0; i < pLen; ++i) {
+            ++count[s.charAt(i) - 'a'];
+            --count[p.charAt(i) - 'a'];
+        }
+
+        int differ = 0;
+        for (int j = 0; j < 26; ++j) {
+            if (count[j] != 0) {
+                ++differ;
             }
         }
-        return new String(s);
+
+        if (differ == 0) {
+            ans.add(0);
+        }
+
+        for (int i = 0; i < sLen - pLen; ++i) {
+            if (count[s.charAt(i) - 'a'] == 1) {  // 窗口中字母 s[i] 的数量与字符串 p 中的数量从不同变得相同
+                --differ;
+            } else if (count[s.charAt(i) - 'a'] == 0) {  // 窗口中字母 s[i] 的数量与字符串 p 中的数量从相同变得不同
+                ++differ;
+            }
+            --count[s.charAt(i) - 'a'];
+
+            if (count[s.charAt(i + pLen) - 'a'] == -1) {  // 窗口中字母 s[i+pLen] 的数量与字符串 p 中的数量从不同变得相同
+                --differ;
+            } else if (count[s.charAt(i + pLen) - 'a'] == 0) {  // 窗口中字母 s[i+pLen] 的数量与字符串 p 中的数量从相同变得不同
+                ++differ;
+            }
+            ++count[s.charAt(i + pLen) - 'a'];
+
+            if (differ == 0) {
+                ans.add(i + 1);
+            }
+        }
+
+        return ans;
     }
 }
