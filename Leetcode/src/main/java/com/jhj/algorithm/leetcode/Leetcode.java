844243a6776646377546f3294071010f83aa0a0c
jhj <hnjzjhj@163.com>
2024-04-10 13:37:41 +0800

    leetcode 63

--- Leetcode/src/main/java/com/jhj/algorithm/leetcode/Leetcode.java
+++ Leetcode/src/main/java/com/jhj/algorithm/leetcode/Leetcode.java
@@ -1,20 +1,29 @@
 package com.jhj.algorithm.leetcode;
 
 class Solution {
-    public int minPathSum(int[][] grid) {
-        int m = grid.length;
-        int n = grid[0].length;
-        int[][] f = new int[m][n];
-        f[0][0]=grid[0][0];
-        for(int i=1;i<n;i++){
-            f[0][i]=f[0][i-1]+grid[0][i];
+    public int uniquePathsWithObstacles(int[][] obstacleGrid) {
+        if(obstacleGrid[0][0]==1){
+            return 0;
         }
-        for(int j=1;j<m;j++){
-            f[j][0]=f[j-1][0]+grid[j][0];
+        int m=obstacleGrid.length;
+        int n=obstacleGrid[0].length;
+        int[][] f = new int[m][n];
+        f[0][0]=1;
+        for(int i=1;i<n;i++){
+            if(obstacleGrid[0][i]==0){
+                f[0][i]=f[0][i-1];
+            }
+        }
+        for(int i=1;i<m;i++){
+            if(obstacleGrid[i][0]==0){
+                f[i][0]=f[i-1][0];
+            }
         }
         for(int i=1;i<m;i++){
             for(int j=1;j<n;j++){
-                f[i][j]=Math.min(f[i-1][j],f[i][j-1])+grid[i][j];
+                if(obstacleGrid[i][j]==0) {
+                    f[i][j] = f[i][j - 1] + f[i - 1][j];
+                }
             }
         }
         return f[m-1][n-1];
