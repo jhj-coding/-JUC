36db15c03d2cd5df998c972f9b7c85f66fa6fdfc
jhj <hnjzjhj@163.com>
2024-04-16 17:25:36 +0800

    快速排序

--- Leetcode/src/main/java/com/jhj/algorithm/sort/Main.java
+++ Leetcode/src/main/java/com/jhj/algorithm/sort/Main.java
@@ -3,7 +3,7 @@ package com.jhj.algorithm.sort;
 public class Main {
     public static void main(String[] args) {
         int[] ints = {5, 2, 3, 6, 1, 4, 9};
-        int[] ints1 = sort1(ints);
+        int[] ints1 = sort2(ints,0,ints.length-1);
         for(int i=0;i<ints1.length;i++){
             System.out.println(ints1[i]);
         }
@@ -20,6 +20,31 @@ public class Main {
                     nums[i+1]=temp;
                 }
             }
+        }
+        return nums;
+    }
+    //快速排序
+    public static int[] sort2(int[] nums,int l,int r){
+        if(l<r){
+            int i,j,x;
+            i=l;
+            j=r;
+            x=nums[i];
+            while (i<j){
+                while (i<j&&nums[j]>x){
+                    j--;
+                }
+                if(i<j)
+                    nums[i++]=nums[j];
+                while (i<j&&nums[i]<x){
+                    i++;
+                }
+                if(i<j)
+                    nums[j--]=nums[i];
+            }
+            nums[i]=x;
+            sort2(nums,l,i-1);
+            sort2(nums,i+1,r);
         }
         return nums;
     }
