293d5119e3796d0a2e9f2ad1de8bb9fb3f60c985
jhj <hnjzjhj@163.com>
2024-04-18 15:50:56 +0800

    插入排序

--- Leetcode/src/main/java/com/jhj/algorithm/sort/Main.java
+++ Leetcode/src/main/java/com/jhj/algorithm/sort/Main.java
@@ -3,7 +3,9 @@ package com.jhj.algorithm.sort;
 public class Main {
     public static void main(String[] args) {
         int[] ints = {5, 2, 3, 6, 1, 4, 9};
-        int[] ints1 = sort2(ints,0,ints.length-1);
+//        int[] ints1 = sort1(ints);
+//        int[] ints1 = sort2(ints,0,ints.length-1);
+        int[] ints1 = sort3(ints);
         for(int i=0;i<ints1.length;i++){
             System.out.println(ints1[i]);
         }
@@ -45,6 +47,25 @@ public class Main {
             nums[i]=x;
             sort2(nums,l,i-1);
             sort2(nums,i+1,r);
+        }
+        return nums;
+    }
+    //插入排序
+    public static int[] sort3(int[] nums){
+        int i,j,k;
+        for(i=1;i<nums.length;i++){
+            for (j=i-1;j>=0;j--){
+                if(nums[i]>nums[j]){
+                    break;
+                }
+            }
+            if(j!=i-1){
+                int temp=nums[i];
+                for(k=i-1;k>j;k--){
+                    nums[k+1]=nums[k];
+                }
+                nums[k+1]=temp;
+            }
         }
         return nums;
     }
